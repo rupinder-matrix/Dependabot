da092bb5a285e341777760a09a5e5a196593a195
rupinder-matrix <119496387+rupinder-matrix@users.noreply.github.com>
2025-01-06 10:15:39 +0530

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "YourPackageName",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "IQKeyboardManagerSwift",
+            targets: ["IQKeyboardManagerSwift"]
+        ),
+    ],
+    dependencies: [
+        // Adding IQKeyboardManagerSwift dependency
+        .package(url: "https://github.com/hackiftekhar/IQKeyboardManager.git", .upToNextMajor(from: "6.5.16"))
+    ],
+    targets: [
+        .target(
+            name: "IQKeyboardManagerSwift",
+            dependencies: [
+                "IQKeyboardManagerSwift"
+            ],
+            path: "Sources"
+        )
+    ]
+)
